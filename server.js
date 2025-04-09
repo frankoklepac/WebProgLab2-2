c7fdcc4a2ebbbb123f1db2a659eda5a0564211f0
frankoklepac <itsmefranko1@gmail.com>
2025-04-09 10:14:35 +0200

    Changed server.js

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+app.use(express.static('public')); 
+
 const PORT = process.env.PORT || 3000;
 app.get('/', (req, res) => {
 res.send('Pozdrav sa Railway servera!');
 });
 app.listen(PORT, () => {
-console.log('Server pokrenut na portu ${PORT}');
+console.log(`Server pokrenut na portu ${PORT}`);
 });
+
